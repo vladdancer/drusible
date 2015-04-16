0928be38540e75a64a020764dd3c27034cc7e26c
Valentine94 <mve@drupalway.net>
2015-04-16 11:22:53 +0300

    Fix PHPStorm_templates install script

--- phpstorm_templates.sh
+++ phpstorm_templates.sh
@@ -1,20 +1,17 @@
 #!/usr/bin/env bash
-# If PHPStorm_templates not exist - install it, otherwise update.
+# If PHPStorm_templates not exist - install it, update otherwise.
 if [ ! -d ~/drusible/PHPStorm_templates ]; then
-  # Create directory for PHPStorm templates.
-  mkdir ~/drusible/PHPStorm_templates
   # Clone PHPStorm templates repo into PHPStorm_templates directory.
-  cd ~/drusible/PHPStorm_templates/
-  git clone https://github.com/Sweetchuck/WebIdeConfigManager.git
-  # Clone PHPStorm templates for Drupal
-  cd WebIdeConfigManager/ConfigHome
-  git clone --recursive --branch 7.x-1.x http://git.drupal.org/project/phpstorm_templates.git drupal
+  git clone https://github.com/Sweetchuck/WebIdeConfigManager.git ~/drusible/PHPStorm_templates
+  # Clone PHPStorm templates for Drupal.
+  git clone --recursive -b 7.x-1.x http://git.drupal.org/project/phpstorm_templates.git ~/drusible/PHPStorm_templates/ConfigHome/drupal
   # Push new templates to apply it to the PHPStorm IDE.
-  cd ..
-  ./WebIdeConfigManager.php push
-  else
-    cd ~/drusible/PHPStorm_templates/WebIdeConfigManager/ConfigHome/drupal
-    git pull
-    cd ../..
-    ./WebIdeConfigManager.php push -h drupal
+  ~/drusible/PHPStorm_templates/WebIdeConfigManager.php push
+else
+  cd ~/drusible/PHPStorm_templates/ConfigHome/drupal
+  git pull
+  cd ../..
+  ./WebIdeConfigManager.php push -h drupal
+  # Go back to the home directory.
+  cd ~
 fi
